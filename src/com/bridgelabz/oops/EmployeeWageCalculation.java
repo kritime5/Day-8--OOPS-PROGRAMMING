5bbf4dac4636e85337411d4413f4dd8db31fe527
Kritika <kritsme963@gmail.com>
2022-11-02 09:52:01 -0700

    Added:add part time

--- src/com/bridgelabz/oops/EmployeeWageCalculation.java
+++ src/com/bridgelabz/oops/EmployeeWageCalculation.java
@@ -13,13 +13,29 @@ public class EmployeeWageCalculation {
             // WAGE-given wage per hr is 20 and full day hr is 8
             //CONSTANTS
             int EMP_WAGE_PER_HOUR=20;
-            int IS_FULL_TIME=1;
+
             //VARIABLES
             int empHrs=0;
             int empWage=0;
 
             empWage=empHrs*EMP_WAGE_PER_HOUR;
             System.out.println("Emp Wage:"+empWage);
+
+            //UC3-ADD PART TIME EMPLOYEE AND WAGE-PART TIME HR IS 8
+            //CONSTANTS
+            int IS_PART_TIME=1;
+            int IS_FULL_TIME=2;
+
+            if(a==1) {
+                System.out.println("The employee is present");
+                if(a==IS_PART_TIME)
+                    empHrs=4;
+                else if (a==IS_FULL_TIME)
+                    empHrs=8;
+            }else {
+                System.out.println("Employee is absent");
+                empHrs=0;
+            }
 
         }
     }
