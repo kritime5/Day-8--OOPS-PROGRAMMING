dfc75f8406ed180eb99b95fda584a4a38da6b50a
Kritika <kritsme963@gmail.com>
2022-11-02 08:46:09 -0700

    Added:Employee wage computation

---
+++ src/com/bridgelabz/oops/EmployeeWageCalculation.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.oops;
+
+public class EmployeeWageCalculation {
+    public static void main(String[] args) {
+        System.out.println("Welcome to employee wage computation");
+    }
+}
